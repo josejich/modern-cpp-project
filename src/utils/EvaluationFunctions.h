5b4741e8eb798240fb8229bcb95ae435ba5bc155
Jose Jimenez <josejich@gmail.com>
2023-03-08 15:40:45 +0100

    Update calculation with more operations

--- src/utils/EvaluationFunctions.h
+++ src/utils/EvaluationFunctions.h
@@ -8,6 +8,7 @@ constexpr float FunctionCompileTime(float _x) {
   float a = 0;
   for (int i = 0; i < 10000; i++) {
     a += i * 0.0001 * _x;
+    a -= _x * _x;
   }
   return a;
 }
@@ -21,6 +22,7 @@ static float FunctionRuntime(float _x) {
   float a = 0;
   for (int i = 0; i < 10000; i++) {
     a += i * 0.0001 * _x;
+    a -= _x * _x;
   }
   return a;
 }
