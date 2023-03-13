6245369c6d87cc19c3dd5a8bab07d9d48145bae0
Jose Jimenez <josejich@gmail.com>
2023-03-13 12:28:49 +0100

    Add documentation

--- src/implementation/ConstExprCalculation.cpp
+++ src/implementation/ConstExprCalculation.cpp
@@ -1,14 +1,15 @@
 #include "ConstExprCalculation.h"
 
-#include "utils/LookUpTable.h"
 #include "utils/EvaluationFunctions.h"
+#include "utils/LookUpTable.h"
 
-namespace{
+namespace {
 using namespace LookUpTable;
-auto table = GenerateTable<target_range_samples>(FunctionCompileTime, target_range_min, target_range_step_size);
-}
+auto table = GenerateTable<target_range_samples>(
+    FunctionCompileTime, target_range_min, target_range_step_size);
+} // namespace
 
 float ConstExprCalculation::PerformCalculation(float input) {
-using namespace LookUpTable;
-return TableLookWithInterpolation<decltype(table)&>(input, table);
+  using namespace LookUpTable;
+  return TableLookWithInterpolation<decltype(table) &>(input, table);
 }
--- src/implementation/ConstExprCalculation.h
+++ src/implementation/ConstExprCalculation.h
@@ -2,9 +2,14 @@
 
 #include "interface/Calculation.h"
 
-
+//!  RunTime Calculation implementation
+/*!
+  This calculation performs an operation at compile time, creates a
+  table of results that can be accessed at execution time.
+*/
 class ConstExprCalculation : public ICalculation {
 public:
-    ~ConstExprCalculation() = default;
-    virtual float PerformCalculation(float input) override;
+  ConstExprCalculation() = default;
+  ~ConstExprCalculation() = default;
+  virtual float PerformCalculation(float input) override;
 };
